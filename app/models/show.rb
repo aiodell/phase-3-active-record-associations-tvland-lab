35b951809b797a28ccc0aff811985ac2093dd975
Andrianna <andrianna.odell@gmail.com>
2022-08-17 14:22:25 -0400

    commit tree b8cf1e4cbff566b0ad088c76180ab8fbc5e83837

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  # map the full names of the characters associated with the show
+  # use the existing full_name method from actor to save time and space
+  def actors_list
+    self.actors.map do |actor|
+      actor.full_name
+    end
+  end
+
+
+
 end
